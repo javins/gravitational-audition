300eb000af15a93cd9f7d874449f5d000aeb1996
Walt Della <walt@javins.net>
2019-10-18 16:10:07 -0700

    Add a comment about lack of cli parsing.

--- src/grav/main.py
+++ src/grav/main.py
@@ -7,6 +7,7 @@ def main():
     The primary entrypoint for this project, and one that can be used in the
     `console_scripts` directive in setup.py.
     """
+    # future work: cli args parsing and --help
     tests = TestLoader().loadTestsFromModule(grav.test)
     # verbosity 2 is enough to show all tests, per:
     # https://github.com/python/cpython/blob/ecb035cd14c11521276343397151929a94018a22/Lib/unittest/runner.py#L40
